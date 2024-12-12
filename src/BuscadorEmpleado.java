821a719b860dc38110ef37c2916b4889d1cf962d
jaesparz <jaesparz@espol.edu.ec>
2024-12-12 16:09:12 -0500

    Inicio Taller

---
+++ src/BuscadorEmpleado.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.util.List;
+
+public class BuscadorEmpleado {
+    public static Empleado buscarEmpleadoPorNombre(String nombre, List<Empleado> empleados) {
+        for (Empleado empleado : empleados) {
+            if (empleado.getNombre().equals(nombre)) {
+                return empleado;
+            }
+        }
+        return null;
+    }
+
+    // Más metodos
+}
